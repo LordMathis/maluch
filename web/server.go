723a3faba99e2c41f8e249dc0c6dba4314b89453
LordMathis <matus@namesny.com>
2023-01-26 19:37:44 +0100

    Implement web server

---
+++ web/server.go
@@ -0,0 +1,20 @@
+package web
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	// Ping test
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "We got Gin!")
+	})
+
+	return r
+}
